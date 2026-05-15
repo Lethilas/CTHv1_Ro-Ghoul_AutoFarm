b9480ef9f530891d78a283701937235d49528805
kyla64 <39499227+kyla64@users.noreply.github.com>
2026-05-15 13:21:09 +0200

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+
+if getgc then
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lethilas/CTHv1_Ro-Ghoul_AutoFarm/refs/heads/main/CTHv1.lua", true))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lethilas/CTHv1_Ro-Ghoul_AutoFarm/refs/heads/main/CTHv1SolaraAndXeno.lua", true))()
+end
